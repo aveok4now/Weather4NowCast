d4cc2e457a1223fcd6289f847ba6f3d61a9b31a9
Vyacheslav <slavach@imperialeads.com>
2024-07-01 22:16:58 +0300

    chore: change default city to sev

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE_TITLE = "Weather4Cast";
 export const SITE_DESCRIPTION = "Basic weather forecast app.";
 
-export const DEFAULT_CITY = "Moscow";
+export const DEFAULT_CITY = "Sevastopol";
 export const DEFAULT_COUNTRY = "Russia";
 
 export const AUTHOR_GH_LINK = "https://github.com/aveok4now";
