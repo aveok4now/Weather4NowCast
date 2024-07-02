7a2cc5f38fafa8c47e1dfda283f95340fe2e5566
2mpain <slavianin898@gmail.com>
2024-07-02 18:47:46 +0300

    style: add particles component

--- src/components/MarqueeCard.tsx
+++ src/components/MarqueeCard.tsx
@@ -26,6 +26,7 @@ export const MarqueeCard = ({
 				colorTo="#73B3FC"
 				duration={10}
 			/>
+
 			<div className="flex flex-row items-center gap-2">
 				<img className="rounded-full" width="32" height="32" alt="" src={img} />
 				<div className="flex flex-col">
@@ -35,6 +36,7 @@ export const MarqueeCard = ({
 					<p className="text-xs font-medium dark:text-white/40">{coords}</p>
 				</div>
 			</div>
+
 			<blockquote className="mt-2 text-sm">{body}</blockquote>
 		</figure>
 	);
