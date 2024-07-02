d51b2886207f6698db77682707297f139fbf6bc7
2mpain <slavianin898@gmail.com>
2024-07-02 18:08:25 +0300

    style: remove masthead margin, marquee cards gradient update

--- src/components/MarqueeeWeatherCards.tsx
+++ src/components/MarqueeeWeatherCards.tsx
@@ -13,9 +13,8 @@ export const MarqueeWeatherCards: React.FC<MarqueeWeatherCardsProps> = ({
 	const secondRow = weatherData.slice(weatherData.length / 2);
 
 	return (
-		<div className="relative flex h-96 flex-row items-center justify-center overflow-hidden rounded-lg bg-background sm:px-20 md:shadow-xl">
-			<div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-transparent dark:from-background"></div>
-			<div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/5 bg-gradient-to-t from-blue-300/60 dark:from-black"></div>
+		<div className="z-50 relative flex h-96 flex-row items-center justify-center overflow-hidden rounded-lg bg-background sm:px-20 md:shadow-xl transition-all duration-300">
+			<div className="pointer-events-none absolute inset-x-0 top-0 h-1/6 bg-gradient-to-b from-white/6s0 dark:from-blue-300/40 "></div>
 			<Marquee pauseOnHover vertical className="[--duration:20s]">
 				{firstRow.map((weather) => (
 					<MarqueeCard key={weather.name} {...weather} />
@@ -26,7 +25,7 @@ export const MarqueeWeatherCards: React.FC<MarqueeWeatherCardsProps> = ({
 					<MarqueeCard key={weather.name} {...weather} />
 				))}
 			</Marquee>
-			<div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-transparent dark:from-background"></div>
+
 			<div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/5 bg-gradient-to-t from-blue-300/60 dark:from-black"></div>
 		</div>
 	);
